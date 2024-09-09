2794a874dbd0bc9c414da500c701617223f087e2
Brian Wastle <brian.wastle1@gmail.com>
2024-09-09 09:33:26 -0400

    added getPortfolio function

--- getPortfolio/index.mjs
+++ getPortfolio/index.mjs
@@ -1,67 +1,117 @@
-import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, QueryCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
 
 const dynamoClient = new DynamoDBClient({ region: 'us-east-1' });
 
 export const handler = async (event) => {
-    const tableName = process.env.TABLE_NAME; 
-    const partitionKey = 'userID'; 
-    const currentDate = new Date();
-    const sortKey = currentDate.toISOString();
-    const { userID, filterValue } = event;
-
-    // Validate input
-    if (typeof userID !== 'string' || typeof filterValue !== 'string') {
+    const dynamicTableName = process.env.ENTRY_TABLE_NAME;
+    const staticTableName = process.env.STATIC_TABLE_NAME;
+    const LSIName = process.env.LSI_NAME;
+    const { userID } = event;
+    
+    if (typeof userID !== 'string') {
         return {
             statusCode: 400,
             body: JSON.stringify({ error: 'Bad Request: Invalid input type.' })
         };
     }
-
+    
     try {
-        // Define query parameters
-        const queryParams = {
-            TableName: tableName,
-            KeyConditionExpression: `${partitionKey} = :userID AND begins_with(${sortKeyPrefix}, :filterValue)`,
+        // Pull user's PORTFOLIO#KEY metadata
+        const portfolioQueryParams = {
+            TableName: dynamicTableName,
+            IndexName: LSIName,
+            KeyConditionExpression: 'userID = :userID AND metadata = :metadata',
             ExpressionAttributeValues: {
                 ':userID': { S: userID },
-                ':filterValue': { S: filterValue }
+                ':metadata': { S: 'PORTFOLIO#KEY' }
             },
-            ScanIndexForward: true, // Controls the order of results
-            Limit: 10 // Limit the number of results returned
+            ScanIndexForward: false,
+            Limit: 1,
+            ConsistentRead: true
         };
+        const portfolioQueryCommand = new QueryCommand(portfolioQueryParams);
+        const portfolioQueryResult = await dynamoClient.send(portfolioQueryCommand);
+        const portfolio = portfolioQueryResult.Items?.[0]?.portfolio?.SS ?? [];
 
-        console.log("Query Parameters:", queryParams);
-
-        // Execute query
-        const queryCommand = new QueryCommand(queryParams);
-        const queryResult = await dynamoClient.send(queryCommand);
-
-        if (queryResult.Items.length === 0) {
-            console.log("No matching items found.");
-            return {
-                statusCode: 404,
-                body: JSON.stringify({ error: 'No matching items found.' })
+        // Fetch balance and price for each ticker
+        const results = [];
+        for (const ticker of portfolio) {
+            // get latest stock balance
+            const currentTickerQueryParams = {
+                TableName: dynamicTableName,
+                IndexName: LSIName,
+                KeyConditionExpression: 'userID = :userID AND begins_with(metadata, :metadata)',
+                ExpressionAttributeValues: {
+                    ':userID': { S: userID },
+                    ':metadata': { S: `${ticker}#` }
+                },
+                ScanIndexForward: false,
+                ConsistentRead: true
             };
+            let stockBalance = 0;
+            try {
+                const currentTickerQueryCommand = new QueryCommand(currentTickerQueryParams);
+                const currentTickerQueryResult = await dynamoClient.send(currentTickerQueryCommand);
+                const latestTickerTransaction = findMostRecentItem(currentTickerQueryResult.Items, 'date');
+                stockBalance = parseFloat(latestTickerTransaction?.balance?.N ?? '0');
+            } catch (error) {
+                console.error(`DynamoDB error for ticker ${ticker}:`, error);
+            }
+
+            // get current stock price
+            const stockQueryParams = {
+                TableName: staticTableName,
+                KeyConditionExpression: 'ticker = :ticker',
+                ExpressionAttributeValues: {
+                    ':ticker': { S: ticker }
+                },
+                ScanIndexForward: false,
+                Limit: 5,
+                ConsistentRead: true
+            };
+            let stockPrice = null;
+            try {
+                const stockQueryCommand = new QueryCommand(stockQueryParams);
+                const stockQueryResult = await dynamoClient.send(stockQueryCommand);
+                const latestStockPrice = findMostRecentItem(stockQueryResult.Items, 'date');
+                stockPrice = parseFloat(latestStockPrice?.price?.N ?? '0');
+            } catch (error) {
+                console.error(`DynamoDB error for ticker ${ticker}:`, error);
+            }
+
+            // Push the results
+            results.push({ ticker, balance: stockBalance, price: stockPrice });
         }
 
-        console.log("Query Result:", queryResult.Items);
-
-        // Return successful response
         return {
             statusCode: 200,
-            body: JSON.stringify(queryResult.Items),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            body: JSON.stringify(results)
         };
+        
     } catch (error) {
         console.error('Error:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Internal Server Error' }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            body: JSON.stringify({ error: 'Internal Server Error' })
         };
     }
+
+    function findMostRecentItem(items, dateField) {
+        if (!Array.isArray(items) || items.length === 0) {
+            return null;
+        }
+        return items.reduce((latest, item) => {
+            const itemDate = item[dateField]?.S; // Access the date field
+            const latestDate = latest ? latest[dateField]?.S : null;
+            if (!latestDate || (itemDate && itemDate > latestDate)) {
+                return item;
+            }
+            return latest;
+        }, null);
+    }
 };
+    
+
+
+    
+
